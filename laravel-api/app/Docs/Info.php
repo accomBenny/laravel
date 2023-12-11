79dcb8e9cc9579aa9723d6cdc3a7993141de216c
accomBenny <cs921029@github.com>
2023-12-11 17:29:35 +0800

    swagger usable

--- laravel-api/app/Docs/Info.php
+++ laravel-api/app/Docs/Info.php
@@ -5,11 +5,48 @@ namespace App\Docs;
 /**
  *  @OA\Info(
  *      version="1.0.0",
- *      title="SHD API Demo",
- *      description="SHD API 文件 <br> 當有其他 API 文件建立之時，務必要將 @OA\PathItem() 這參數以及此描述刪除",
- *  )
+ *      title="Board API Demo",
+ *      description="留言板API 文件",
+ *      @OA\Contact(
+ *          name="行雲者",
+ *          url="https://www.cmrdb.cs.pu.edu.tw",
+ *          email="developer@example.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      ),
+ *  ),
+ *  // 當有底下有 API Get,Post,... 就可以刪掉
+ *  // 是為了展示 Info 用途而已，不然會出 Error
  *  @OA\PathItem(
  *      path="/"
+ *  ),
+ *  *  @OA\SecurityScheme(
+ *      securityScheme="bearerAuth",
+ *      type="http",
+ *      scheme="bearer",
+ *      description="API accessToken"
+ *  ),
+ *  @OA\SecurityScheme(
+ *      securityScheme="Authorization",
+ *      type="apiKey",
+ *      in="header",
+ *      name="Authorization_name"
+ *  ),
+ *  @OA\SecurityScheme(
+ *      securityScheme="Cookie",
+ *      type="apiKey",
+ *      in="cookie",
+ *      description="Cookie description",
+ *      name="Cookie_name"
+ *  ),
+ *  @OA\SecurityScheme(
+ *      securityScheme="Query",
+ *      type="apiKey",
+ *      in="query",
+ *      description="Query description",
+ *      name="Query_name"
  *  )
  */
 class Info
--- laravel-api/app/Providers/RouteServiceProvider.php
+++ laravel-api/app/Providers/RouteServiceProvider.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
@@ -12,16 +10,13 @@ class RouteServiceProvider extends ServiceProvider
 {
     /**
      * The path to the "home" route for your application.
-     *
      * Typically, users are redirected here after authentication.
-     *
      * @var string
      */
     public const HOME = '/home';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
-     *
      * @return void
      */
     public function boot()
@@ -37,10 +32,8 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/web.php'));
         });
     }
-
     /**
      * Configure the rate limiters for the application.
-     *
      * @return void
      */
     protected function configureRateLimiting()
